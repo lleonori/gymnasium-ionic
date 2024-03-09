3ab1bc066547ce656892e571710d590e35dc2902
lleonori <lorenzo.leonori.93@gmail.com>
2024-03-09 15:17:33 +0100

    T0; init project

---
+++ android/app/src/main/java/gymnasium/ionic/MainActivity.java
@@ -0,0 +1,5 @@
+package gymnasium.ionic;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gymnasium.ionic',
+  appName: 'gymnasium-ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
