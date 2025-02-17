4ff6770564834da4e63f05ee709358d27da47258
lleonori <lorenzo.leonori.93@gmail.com>
2025-02-17 15:27:37 +0100

    Handle ssl based on env variable

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,17 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const config: CapacitorConfig = {
   appId: "gymnasium.ionic",
   appName: "gymnasium-ionic",
   webDir: "dist",
-  server: {
-    androidScheme: "https",
-    iosScheme: "https",
-  },
+  server: isProduction
+    ? {
+        androidScheme: "https",
+        iosScheme: "https",
+      }
+    : { androidScheme: "http", iosScheme: "http" },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import * as fs from "fs";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
   server: {
-    https: {
-      key: fs.readFileSync("./ssl/localhost.key"),
-      cert: fs.readFileSync("./ssl/localhost.crt"),
-    },
+    https: isProduction
+      ? {
+          key: fs.readFileSync("./ssl/localhost.key"),
+          cert: fs.readFileSync("./ssl/localhost.crt"),
+        }
+      : {},
   },
 });
